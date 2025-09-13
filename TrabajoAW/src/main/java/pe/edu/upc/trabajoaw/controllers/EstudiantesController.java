9d60c8aec21b13b63245f9ae4c3dd4e96d8b843b
JuanSudo <u202211414@upc.edu.pe>
2025-09-12 19:14:54 -0500

    Se quito un import

--- TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/controllers/EstudiantesController.java
+++ TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/controllers/EstudiantesController.java
@@ -3,7 +3,6 @@ package pe.edu.upc.trabajoaw.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.trabajoaw.dtos.EstudiantesDTO;
