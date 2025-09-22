cf8eff8285741fecc08e8ecc272210e86ab17635
Joaquin Silva <joaquinsilvasistemas@gmail.com>
2025-09-22 15:40:57 -0500

    hizo un query

--- TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/repositories/IAlertaRepository.java
+++ TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/repositories/IAlertaRepository.java
@@ -1,7 +1,16 @@
 package pe.edu.upc.trabajoaw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.edu.upc.trabajoaw.entities.Alerta;
 
+import java.util.List;
+
 public interface IAlertaRepository extends JpaRepository<Alerta, Integer> {
+    @Query (value = "SELECT s.id_Sitio_Web, s.nombre, COUNT(sa.id_Alerta) AS alertas_asociadas\n" +
+            "FROM Sitios_Web s\n" +
+            "LEFT JOIN Sitios_Web_Alertas sa ON sa.id_Sitio_Web = s.id_Sitio_Web\n" +
+            "GROUP BY s.id_Sitio_Web, s.nombre\n" +
+            "ORDER BY alertas_asociadas DESC, s.id_Sitio_Web", nativeQuery = true)
+    public List<Alerta> cantidadSitiosWeb();
 }
