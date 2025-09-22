1807df44ab1c58626b18fc99a6f4e522fa1382e2
Joaquin Silva <joaquinsilvasistemas@gmail.com>
2025-09-22 15:27:32 -0500

    hizo un query

--- TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/repositories/IRecordatorioRepository.java
+++ TrabajoAW/src/main/java/pe/edu/upc/trabajoaw/repositories/IRecordatorioRepository.java
@@ -1,7 +1,15 @@
 package pe.edu.upc.trabajoaw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.edu.upc.trabajoaw.entities.Recordatorio;
 
+import java.util.List;
+
 public interface IRecordatorioRepository extends JpaRepository<Recordatorio, Integer> {
+    @Query(value = "SELECT r.id_Recordatorio, r.titulo, r.descripcion, r.dia_Elegido, r.activo\n" +
+            "FROM Recordatorio r\n" +
+            "WHERE r.activo = 'SI' AND r.dia_Elegido = 'Lunes'\n" +
+            "ORDER BY r.id_Recordatorio", nativeQuery = true)
+    public List<String[]> cantidadRecordatoriosActivospordia();
 }
