2c86338e3a9ef3fd7abce2bc53b7700eda3598a1
Jose Castillo <ammajo777@gmail.com>
2025-11-30 15:23:39 -0500

    implementacion de css

--- FrontAW/src/app/components/menu/menu.ts
+++ FrontAW/src/app/components/menu/menu.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { Loginservice } from '../../services/loginservice';
 
 @Component({
   selector: 'app-menu',
@@ -12,5 +13,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './menu.css',
 })
 export class Menu {
+  role: string = '';
+  usuario: string = '';
 
+  constructor(private loginService: Loginservice) {}
+
+  cerrar() {
+    sessionStorage.clear();
+  }
+  
+ 
+  verificar() {
+    this.role = this.loginService.showRole();
+
+    return this.loginService.verificar();
+  }
+  isAdmin() {
+    return this.role === 'ADMIN';
+  }
+
+  isTester() {
+    return this.role === 'TESTER';
+  }
 }
