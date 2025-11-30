771b51f134a9ac8910d40509f90e850e550f7635
Jose Castillo <ammajo777@gmail.com>
2025-11-30 17:49:00 -0500

    implementacion API musica

--- FrontAW/src/app/components/home/home.ts
+++ FrontAW/src/app/components/home/home.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { Deezer } from "../deezer/deezer";
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule, MatIconModule, RouterLink, CommonModule],
+  imports: [MatCardModule, MatIconModule, RouterLink, CommonModule, Deezer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  
 }
